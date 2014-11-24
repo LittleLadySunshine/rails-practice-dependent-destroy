355f5b734c91a420e0659ebaadbdef57766061d7
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-11-24 11:40:59 -0700

    delete

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -36,7 +36,7 @@ class Prescription < ActiveRecord::Base
   def dosage_starts_with_number
     if dosage.present? && !dosage.match(/^\d/)
       errors.add(:dosage, "must start with a number")
-    end 
+    end
   end
 
   def starts_on_later_than_ends_on
@@ -46,5 +46,4 @@ class Prescription < ActiveRecord::Base
       errors.add(:starts_on, "must be earlier than ends on")
     end
   end
-
 end
